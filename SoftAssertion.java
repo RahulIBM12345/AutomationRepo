9e9a77b7e91fdebc91e34c24a46818f3229254d1
RahulIBM12345 <rp9109150@Gmail.com>
2024-05-19 13:28:41 +0530

    Add files via upload

---
+++ SoftAssertion.java
@@ -0,0 +1,37 @@
+package testng.assertion.softassertion;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SoftAssertion {
+
+    @Test
+    public void assertEqualMethod() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+        SoftAssert a = new SoftAssert();
+        String title = driver.getTitle();
+        a.assertEquals(title, "Google", "title should be match");
+        a.assertAll();
+        driver.close();
+    }
+
+    @Test
+    public void A() {
+
+        String exeTest = "hi";
+        String actTest = "hii";
+        SoftAssert a = new SoftAssert();
+        a.assertEquals(exeTest,actTest);
+        a.assertAll();
+    }
+
+    @Test
+    public void B() {
+        System.out.println("Hi");
+    }
+}
